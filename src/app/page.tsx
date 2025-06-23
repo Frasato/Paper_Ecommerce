1abdf5b597d6a36f595772a2d5d46f03004c6c09
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-23 16:12:09 -0300

    :sparkles: feat: Create Login Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-const Home = () => {
+import "@/styles/login.module.scss";
+
+export default function Login(){
   return(
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <main>
+      <div>
+        <div>
+          <h1>Olá, bem-vindo de volta</h1>
+          <h2>A nossa papelaria</h2>
+        </div>
+        <form>
+          <label>Email</label>
+          <input type="text"/>
+          <label>Senha</label>
+          <input type="password"/>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    </main>
   );
 }
-
-export default Home;
