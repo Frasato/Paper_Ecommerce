533b96158d283c7509d886e9a46c53668538ece4
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-06-23 16:11:34 -0300

    :sparkles: feat: Create types for auth

---
+++ src/types/authType.ts
@@ -0,0 +1,34 @@
+export interface User{
+    name: string;
+    userId: string;
+    token: string
+}
+
+export interface LoginProps{
+    email: string;
+    password: string;
+}
+
+export interface RegisterProps{
+    name: string;
+    password: string;
+    email: string;
+    phone: string;
+    cpf: string;
+    street: string;
+    number: string;
+    countryState: string;
+    district: string;
+    city: string;
+    cep: string;
+}
+
+export interface UseAuthReturn{
+    user: User | null;
+    isLoading: boolean;
+    error: string | null;
+    loginUser: (credentials: LoginProps) => Promise<void>;
+    registerUser: (userData: RegisterProps) => Promise<void>;
+    logout: () => void;
+    clearError: () => void;
+}
