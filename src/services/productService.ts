d887229bacf930cbee1658d0927809ffd52a5e8f
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-07-01 13:14:21 -0300

    :sparkles: feat: Fetch category and purchase products

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -36,3 +36,37 @@ export async function getProductById(id: string): Promise<Product>{
 
     throw new Error(`Product not found on id: ${id}`);
 }
+
+export async function getProductByCategory(category: string): Promise<Product[]>{
+    const response = await fetch(`${process.env.URL_API}/product/cat/${category}`, {
+        method: "GET",
+        headers: {
+            "Content-Type":"application/json",
+            "Authorization":`Bearer ${user.token}`
+        }
+    });
+
+    if(response.ok){
+        const data = await response.json();
+        return data;
+    }
+
+    throw new Error(`Products not found on category: ${category}`);
+}
+
+export async function getPurchaseProducts(): Promise<Product[]>{
+    const response = await fetch(`${process.env.URL_API}/product/purchase`, {
+        method: "GET",
+        headers: {
+            "Content-Type":"application/json",
+            "Authorization":`Bearer ${user.token}`
+        }
+    });
+
+    if(response.ok){
+        const data = await response.json();
+        return data;
+    }
+
+    throw new Error(`Products not found!`);
+}
