39a5c02f81676fcb2566b79d9909338e2a4fffa9
Gabriel Frasato <gabriel.frasato@hotmail.com>
2025-07-16 00:31:50 -0300

    :lipstick: feat: Create styles for auth form fields

--- src/components/authFormFields.tsx
+++ src/components/authFormFields.tsx
@@ -1,17 +1,25 @@
 import { AuthFormFieldType } from "@/types/authFormFieldType";
+import "@/styles/authFormFields.scss";
 
 const AuthFormFields = (props: AuthFormFieldType) => {
     
     const Icon = props.icon;
     
     return(
-        <div>
-            <label>{props.label}</label>
-            <div>
-                <Icon />
-                <input type={props.type} value={props.value} onChange={props.onChange}/>
-            </div>
+    <div className="auth-form-field">
+        <label className="auth-field-label">{props.label}</label>
+        <div className="auth-field-container">
+            <Icon className="auth-field-icon" />
+            <input 
+            className="auth-field-input"
+            type={props.type} 
+            value={props.value} 
+            onChange={props.onChange}
+            placeholder={props.label}
+            required
+            />
         </div>
+    </div>
     )
 }
 
