8eca2be88ff1aa26aff13530e7f8de9a98a3744f
Frasato <gabriel.frasato@hotmail.com>
2026-02-10 11:06:11 -0300

    refactor(service): get the token on cart service on server side rendering

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,11 +1,15 @@
 import { Cart } from "@/types/cartType";
+import { LocalStorageUser } from "@/types/userType";
+import { getLocalStorageUser } from "@/utils/getLocalStorage";
 
-export async function getUserCart(userId: string, token: string): Promise<Cart>{
+export async function getUserCart(userId: string): Promise<Cart>{
+    const localUser: LocalStorageUser = getLocalStorageUser();
+
     const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/cart/${userId}`, {
         method: "GET",
         headers: {
             'Content-Type':"application/json",
-            "Authorization":`Bearer ${token}`
+            "Authorization":`Bearer ${localUser.token}`
         }
     });
 
@@ -17,13 +21,15 @@ export async function getUserCart(userId: string, token: string): Promise<Cart>{
     throw new Error(`Failed to load cart by user with id: ${userId}`)
 }
 
-export async function addItemOnCart(productId: string, userId: string, token: string){
+export async function addItemOnCart(productId: string, userId: string){
+    const localUser: LocalStorageUser = getLocalStorageUser();
+
     try{
         await fetch(`${process.env.NEXT_PUBLIC_URL_API}/cart`, {
             method: "POST",
             headers: {
                 "Content-Type":"application/json",
-                "Authorization":`Bearer ${token}`
+                "Authorization":`Bearer ${localUser.token}`
             },
             body: JSON.stringify({
                 userId: userId,
@@ -35,13 +41,15 @@ export async function addItemOnCart(productId: string, userId: string, token: st
     }
 }
 
-export async function plusOneOnItem(cartItemId: string, token: string){
+export async function plusOneOnItem(cartItemId: string){
+    const localUser: LocalStorageUser = getLocalStorageUser();
+
     try{
         await fetch(`${process.env.NEXT_PUBLIC_URL_API}/cart/plus/${cartItemId}`, {
             method: "PUT",
             headers: {
                 "Content-Type":"application/json",
-                "Authorization":`Bearer ${token}`
+                "Authorization":`Bearer ${localUser.token}`
             },
         });
     }catch(err){
@@ -49,13 +57,15 @@ export async function plusOneOnItem(cartItemId: string, token: string){
     }
 }
 
-export async function minusOneOnItem(cartItemId: string, token: string){
+export async function minusOneOnItem(cartItemId: string){
+    const localUser: LocalStorageUser = getLocalStorageUser();
+
     try{
         await fetch(`${process.env.NEXT_PUBLIC_URL_API}/cart/minus/${cartItemId}`, {
             method: "PUT",
             headers: {
                 "Content-Type":"application/json",
-                "Authorization":`Bearer ${token}`
+                "Authorization":`Bearer ${localUser.token}`
             },
         });
     }catch(err){
@@ -63,13 +73,15 @@ export async function minusOneOnItem(cartItemId: string, token: string){
     }
 }
 
-export async function removeItemCart(cartItemId: string, token: string){
+export async function removeItemCart(cartItemId: string){
+    const localUser: LocalStorageUser = getLocalStorageUser();
+
     try{
         await fetch(`${process.env.NEXT_PUBLIC_URL_API}/cart/${cartItemId}`, {
             method: "PUT",
             headers: {
                 "Content-Type":"application/json",
-                "Authorization":`Bearer ${token}`
+                "Authorization":`Bearer ${localUser.token}`
             },
         });
     }catch(err){
@@ -77,13 +89,15 @@ export async function removeItemCart(cartItemId: string, token: string){
     }
 }
 
-export async function clearCart(cartId: string, token: string){
+export async function clearCart(cartId: string){
+    const localUser: LocalStorageUser = getLocalStorageUser();
+    
     try{
         await fetch(`${process.env.NEXT_PUBLIC_URL_API}/cart/clear/${cartId}`, {
             method: "PUT",
             headers: {
                 "Content-Type":"application/json",
-                "Authorization":`Bearer ${token}`
+                "Authorization":`Bearer ${localUser.token}`
             },
         });
     }catch(err){
